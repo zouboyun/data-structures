6858697c14ada6dc5aaf4a85951530bf75fac71e
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-09-19 15:35:58 -0700

    completed es6 stack part

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,30 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-  }
+    this.count = 0;
+    this.storage = {};
+  };
 
+  push(value) {
+    for (var i = this.count - 1; i >= 0; i--) {
+      this.storage[i + 1] = this.storage[i];
+    }
+    this.storage[0] = value;
+    this.count++;
+  };
+
+  pop() {
+    if (this.count > 0) {
+      var result = this.storage[0];
+      for (var i = 0; i < this.count; i++) {
+        this.storage[i] = this.storage[i + 1];
+      }
+      this.count--;
+      return result;
+    }
+  };
+
+  size() {
+    return this.count;
+  };
 }
