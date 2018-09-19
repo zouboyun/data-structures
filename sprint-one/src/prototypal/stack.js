894d76133ba42568702f747f99447ef8ed4ee033
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-09-19 15:14:14 -0700

    completed prototypical stack part

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,33 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack.storage = {};
+  stack.count = 0;
+  return stack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  for (var i = this.count - 1; i >= 0; i--) {
+    this.storage[i + 1] = this.storage[i];
+  }
+  this.storage[0] = value;
+  this.count++;
+};
 
+stackMethods.pop = function() {
+  if (this.count > 0) {
+    var result = this.storage[0];
+    for (var i = 0; i < this.count; i++) {
+      this.storage[i] = this.storage[i + 1];
+    }
+    this.count--;
+    return result;
+  }
+};
+
+stackMethods.size = function() {
+  return this.count;
+};
