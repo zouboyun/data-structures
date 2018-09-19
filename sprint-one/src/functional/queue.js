f3a248b30a32da10a19942d9229eb66c9bf4827b
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-09-19 12:17:37 -0700

    completed queue shared behavior and queue specific behavior

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    for (var i = size - 1; i >= 0; i--) {
+      someInstance[i+1] = someInstance[i];
+    }
+    someInstance[0] = value;
+    size++;
   };
 
   someInstance.dequeue = function() {
+    if (size > 0) {
+      var result = someInstance[size-1];
+      size--;
+      return result;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
