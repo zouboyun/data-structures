0a9d051fff6fbdc03310fb31419c0d90705b6f3e
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-09-19 15:01:22 -0700

    completed functional shared stack part

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,6 +2,7 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var stack = {};
+  stack.storage = {};
   stack.count = 0;
   _.extend(stack, stackMethods);
   return stack;
@@ -11,17 +12,17 @@ var stackMethods = {};
 
 stackMethods.push = function(value) {
   for (var i = this.count - 1; i >= 0; i--) {
-    this[i + 1] = this[i];
+    this.storage[i + 1] = this.storage[i];
   }
-  this[0] = value;
+  this.storage[0] = value;
   this.count++;
 };
 
 stackMethods.pop = function() {
   if (this.count > 0) {
-    var result = this[0];
+    var result = this.storage[0];
     for (var i = 0; i < this.count; i++) {
-      this[i] = this[i + 1];
+      this.storage[i] = this.storage[i + 1];
     }
     this.count--;
     return result;
