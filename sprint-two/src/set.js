597849d298496c0d49695998a6e8701897a5127d
zouboyun <zouboyun@bu.edu>
2018-09-20 15:52:03 -0700

    Implement set data model

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this._storage[item] === undefined) {
+    // debugger;
+    this._storage[item] = null;
+    // return Object.keys(this._storage);
+  }
+  
 };
 
 setPrototype.contains = function(item) {
+  return this._storage[item] !== undefined;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+   .add() is of constant complexity
+   .contains() is of constant complexity
+   .remove() is of constant complexity
  */
