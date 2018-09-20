14f7aef79e316a948ba135d99f7cb66950d8ce8d
zouboyun <zouboyun@bu.edu>
2018-09-20 16:26:54 -0700

    Halfway done with hashTable solution

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,23 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index,v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  debugger;
+  this._storage.each(function(value,i,hashStorage) {
+    debugger;
+    if (value === index) {
+      hashStorage.splice(i,1);
+    }
+  });
 };
 
 
