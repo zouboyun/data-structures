475406c51fefb4a6cfc39a27b243bd66254cb523
zouboyun <zouboyun@bu.edu>
2018-09-21 10:39:46 -0700

    Complete binarySearchTree Class

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,58 @@
 var BinarySearchTree = function(value) {
+  var bst = Object.create(BinarySearchTree.methods);
+  bst.value = value;
+  bst.left = null; // fix it later
+  bst.right = null; // fix it later
+  return bst;
 };
 
+BinarySearchTree.methods = {};
+
+BinarySearchTree.methods.insert = function(value) {
+  // search for the spot to insert
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else if (value > this.value) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+BinarySearchTree.methods.contains = function(target) {
+  if (target === this.value) {
+    return true;
+  } else if (target < this.value) {
+    if (this.left !== null) {
+      return this.left.contains(target);
+    } else {
+      return false;
+    }
+  } else if (target > this.value) {
+    if (this.right !== null) {
+      return this.right.contains(target);
+    } else {
+      return false;
+    }
+  }
+};
+
+BinarySearchTree.methods.depthFirstLog = function(func) {
+  // debugger;
+  func(this.value);
+  if (this.left !== null) {
+    this.left.depthFirstLog(func);
+  }
+  if (this.right !== null) {
+    this.right.depthFirstLog(func);
+  }
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
