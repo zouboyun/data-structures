11b45b9ff687eda10eb4c8b3b4ffbaa8e1ddeb3c
zouboyun <zouboyun@bu.edu>
2018-09-21 11:11:29 -0700

    Include comments with code for HashTable functionality that enables multi-pair storage at index

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,7 +8,7 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = this._hashFunction(k, this._limit);
-  this._storage.set(index,v);
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -18,8 +18,8 @@ HashTable.prototype.retrieve = function(k) {
 
 HashTable.prototype.remove = function(k) {
   var index = this._hashFunction(k, this._limit);
-  this._storage.each(function(value,i,array) {
-    array.splice(index,1);
+  this._storage.each(function(value, i, array) {
+    array.splice(index, 1);
   });
 };
 
@@ -32,4 +32,38 @@ HashTable.prototype.remove = function(k) {
   .remove() has linear complexity
  */
 
+/* ALLOWS MULTIPLE PAIR STORAGE @ SINGLE INDEX */
+// HashTable.prototype.insert = function(k, v) {
+//   var index = this._hashFunction(k, this._limit);
+//   var existingTuples = this._storage.get(index);
+//   var newTuple = [k, v];
+//   if (existingTuples !== undefined || existingTuples.length === 0) {
+//     existingTuples.push(newTuple);
+//   } else {
+//     existingTuples = [newTuple];
+//   }
+//   this._storage.set(index, existingTuples);
+// };
 
+// HashTable.prototype.retrieve = function(k) {
+//   var index = this._hashFunction(k, this._limit);
+//   var existingTuples = this._storage.get(index);
+//   if (existingTuples !== undefined || existingTuples.length === 0) {
+//     for (var i = 0; i < existingTuples.length; i++) {
+//       if (existingTuples[i][0] === k) {
+//         return existingTuples[i][1];
+//       }
+//     }
+//   }
+// };
+
+// HashTable.prototype.remove = function(k) {
+//   var index = this._hashFunction(k, this._limit);
+//   this._storage.each(function(value, i, array) {
+//     for (var i = 0; i < array[index].length; i++) {
+//       if (array[index][i][0] === k) {
+//         array[index].splice(i, 1);
+//       }
+//     }
+//   });
+// };
