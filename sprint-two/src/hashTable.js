caa3ae5e09f6b75d9924b2937be974298452eb2d
zouboyun <zouboyun@bu.edu>
2018-09-21 08:49:52 -0700

    Complete hashTable.js Class

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,20 +3,21 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._hashFunction = window.getIndexBelowMaxForKey;
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this._hashFunction(k, this._limit);
   this._storage.set(index,v);
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this._hashFunction(k, this._limit);
   return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this._hashFunction(k, this._limit);
   this._storage.each(function(value,i,array) {
     array.splice(index,1);
   });
@@ -26,6 +27,9 @@ HashTable.prototype.remove = function(k) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  .insert() has constant complexity
+  .retrieve() has constant complexity
+  .remove() has linear complexity
  */
 
 
