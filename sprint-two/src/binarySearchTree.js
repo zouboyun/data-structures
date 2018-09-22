a344ce82509db365d575c6bd3e7a49755cddfc7d
Boyun Zou <zouboyun@Boyuns-MacBook-Pro.local>
2018-09-22 16:54:31 -0700

    updated binary search tree and hash table

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,27 @@
 var BinarySearchTree = function(value) {
+  var binarytree = Object.create(BinarySearchTree);
+  return binarytree;
 };
 
+BinarySearchTree.left = function() {
+
+};
+
+BinarySearchTree.right = function() {
+
+};
+
+BinarySearchTree.insert = function() {
+
+};
+
+BinarySearchTree.contains = function() {
+
+};
+
+BinarySearchTree.depthFirstLog = function() {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,22 +3,23 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._hashFunction = window.getIndexBelowMaxForKey;
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(index,v);
+  var index = this._hashFunction(k, this._limit);
+  this._storage.set(index, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  var index = this._hashFunction(k, this._limit);
   return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.each(function(value,i,array) {
-    array.splice(index,1);
+  var index = this._hashFunction(k, this._limit);
+  this._storage.each(function(value, i, array) {
+    array.splice(index, 1);
   });
 };
 
